9a5ed48c29fedd4e636cde6c8a03c3e24536ad70
Suleman <sulemanhamza97@gmail.com>
2023-01-14 23:38:37 +0500

    Initial Commit - Boilerplate code + screen setup

---
+++ main.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gdamore/tcell"
+	"os"
+	"time"
+)
+
+const SnakeSymbol = 0x2588
+const AppleSymbol = 0x25CF
+const GameFrameWidth = 30
+const GameFrameHeight = 15
+const GameFrameSymbol = "||"
+
+type GameObject struct {
+	row, col, width, height int
+	velRow, velCol          int
+	symbol                  rune
+}
+
+var screen tcell.Screen
+var isGamePaused bool
+var debugLog string
+
+var gameObjects []*GameObject
+
+func main() {
+	InitScreen()
+	InitGameState()
+	inputChan := InitUserInput()
+
+	for {
+		HandleUserInput(ReadInput(inputChan))
+		UpdateState()
+		DrawState()
+
+		time.Sleep(75 * time.Millisecond)
+	}
+
+	screen.Fini()
+}
+
+func InitScreen() {
+	var err error
+	screen, err = tcell.NewScreen()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+	if e := screen.Init(); e != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", e)
+		os.Exit(1)
+	}
+
+	defStyle := tcell.StyleDefault.
+		Background(tcell.ColorBlack).
+		Foreground(tcell.ColorWhite)
+	screen.SetStyle(defStyle)
+}
+
+func InitGameState() {
+	gameObjects = []*GameObject{}
+}
+
+func InitUserInput() chan string {
+	inputChan := make(chan string)
+	go func() {
+		for {
+			switch ev := screen.PollEvent().(type) {
+			case *tcell.EventKey:
+				inputChan <- ev.Name()
+
+			}
+		}
+	}()
+
+	return inputChan
+}
+
+func ReadInput(inputChan chan string) string {
+	var key string
+
+	select {
+	case key = <-inputChan:
+	default:
+		key = ""
+	}
+
+	return key
+}
+
+func HandleUserInput(key string) {
+	if key == "Rune[q]" {
+		screen.Fini()
+		os.Exit(1)
+	}
+}
+
+func UpdateState() {
+	if isGamePaused {
+		return
+	}
+
+	for i := range gameObjects {
+		gameObjects[i].row += gameObjects[i].velRow
+		gameObjects[i].col += gameObjects[i].velCol
+	}
+}
+
+func DrawState() {
+	if isGamePaused {
+		return
+	}
+
+	screen.Clear()
+
+	PrintString(0, 0, debugLog)
+
+	for _, obj := range gameObjects {
+		PrintFilledRect(obj.row, obj.col, obj.width, obj.height, obj.symbol)
+	}
+
+	screen.Show()
+}
+
+func PrintStringCentered(row, col int, str string) {
+	col = col - len(str)/2
+	PrintString(row, col, str)
+}
+
+func PrintString(row, col int, str string) {
+	for _, c := range str {
+		PrintFilledRect(row, col, 1, 1, c)
+		col += 1
+	}
+}
+
+func PrintFilledRect(row, col, width, height int, ch rune) {
+	for r := 0; r < height; r++ {
+		for c := 0; c < width; c++ {
+			screen.SetContent(col+c, row+r, ch, nil, tcell.StyleDefault)
+		}
+	}
+}
